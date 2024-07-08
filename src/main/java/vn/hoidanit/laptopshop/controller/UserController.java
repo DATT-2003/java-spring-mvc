efbdfaa16d2f43e3ae478526625925beed470282
Tên của bạn <dattien52003@gmail.com>
2024-07-08 13:05:33 +0700

    commit

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,11 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handalHello();
-        return "Page.html";
+        model.addAttribute("Page", test);
+        model.addAttribute("Test1", "Modal");
+        return "Page_1";
     }
 }
 
